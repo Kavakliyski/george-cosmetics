eb1e476976c450f46c44917d9f057ab269a91a4d
luskoeqk <77803201+luskoeqk@users.noreply.github.com>
2023-01-23 12:22:21 +0000

    empty(strapi prep)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,9 @@
 import Head from 'next/head';
 
-import { sanity_client } from '../library/sanity-client';
-import HeroBanner from '../components/landingPage/HeroBanner';
 
 
-interface IProps {
-    bannerData: string;
-}
+export default function Home() {
 
-export default function Home({ bannerData }: IProps) {
-
-  
 
     return (
         <>
@@ -25,21 +18,9 @@ export default function Home({ bannerData }: IProps) {
 
             <main className=''>
                 landing page
-                {/* <HeroBanner heroBanner={bannerData.length && bannerData[0]} /> */}
                 <h2>Banner</h2>
                 <h2>Products</h2>
             </main>
         </>
     )
-}
-
-
-export const getServerSideProps = async () => {
-    const bannerQuery = '*[_type == banner]';
-    const bannerData = await sanity_client.fetch(bannerQuery);
-
-
-    return {
-        props: { bannerData }
-    }
 }
